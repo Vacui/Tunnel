13691ce5d07724094e1b70f56795791adbadf74d
vacui <graizzaromatteo@gmail.com>
2021-03-06 19:23:42 +0100

    refactor: new tile models & map border
    * add floor models
    * add wall models
    * merge utils classes in MyUtils.cs
    * add border option to map
    * add blank tile prefab
    * update tiles prefabs
    * add temporary scene with game style test

--- Assets/Scripts/Base/Map/MapManager.cs
+++ Assets/Scripts/Base/Map/MapManager.cs
@@ -1,11 +1,13 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 
 public class MapManager : MonoBehaviour {
 
     public class Seed {
         public int width;
         public int height;
-        public string[] cells;
+        public List<string> cells;
 
         public bool isValid { get; private set; }
 
@@ -13,18 +15,18 @@ public class MapManager : MonoBehaviour {
 
             width = -1;
             height = -1;
-            cells = new string[0];
+            cells = new List<string>();
 
             seed = seed.Trim();
-            string[] seedParts = seed.Split('/');
+            List<string> seedParts = seed.Split('/').ToList();
 
             isValid = false;
 
-            if (seedParts.Length == 3) {
+            if (seedParts.Count == 3) {
                 if (int.TryParse(seedParts[0], out width)) {
                     if (int.TryParse(seedParts[1], out height)) {
-                        string[] tiles = seedParts[2].Split('-');
-                        if (tiles.Length == width * height) {
+                        List<string> tiles = seedParts[2].Split('-').ToList();
+                        if (tiles.Count == width * height) {
                             cells = tiles;
                             isValid = true;
                         } else {
@@ -41,12 +43,42 @@ public class MapManager : MonoBehaviour {
             }
 
         }
+
+        public void AddBorder(int depth) {
+            if (depth > 0) {
+                List<string> newCells = new List<string>();
+                newCells.AddBlankRange(width + (2 * depth));
+                for (int i = 0; i < cells.Count; i++) {
+                    if (i % width == 0) {
+                        newCells.AddBlankRange(depth);
+                    }
+                    newCells.Add(cells[i]);
+                    if ((i + 1) % width == 0) {
+                        newCells.AddBlankRange(depth);
+                    }
+                }
+                newCells.AddBlankRange(width + (2 * depth));
+                cells = newCells;
+                width += depth * 2;
+                height += depth * 2;
+            }
+        }
+
+        public string GetSeed() {
+            string seed = $"{width}/{height}/";
+            foreach (string cell in cells) {
+                seed += $"{cell}-";
+            }
+            seed.Trim('-');
+            return seed;
+        }
     }
 
     public event System.EventHandler<GridCoordEventArgs> OnGridReady;
 
-    private const float C_CellSize = 1f;
+    private const float C_CellSize = 4f;
 
+    [SerializeField] private int borderDepth = 0;
     public GridXZ<GridObject> grid { get; private set; }
     [SerializeField] private GameObject[] tilesArray;
     private Transform mapParent;
@@ -63,9 +95,12 @@ public class MapManager : MonoBehaviour {
         PlacedObject.OnExploring += (s, e) => { mapTilesExplored++; /*Debug.Log($"Map discovery percentage {System.Math.Truncate((MapExploringPercentage * 100) * 100) / 100}% ({mapTiles}/{mapTilesExplored})");*/ };
     }
 
-    public void LoadMap(Seed seed, Vector3 mapOriginWorldPosition) {
+    public void LoadMap(Seed seed, Vector3 mapOriginWorldPosition, bool addBorder = false) {
 
         if (seed.isValid) {
+
+            if (addBorder) seed.AddBorder(borderDepth);
+
             if (mapParent != null) {
                 Destroy(mapParent.gameObject);
             }
@@ -82,12 +117,13 @@ public class MapManager : MonoBehaviour {
             grid = new GridXZ<GridObject>(seed.width, seed.height, C_CellSize, mapOriginWorldPosition, (GridXZ<GridObject> g, int x, int z) => new GridObject(g, x, z));
             grid.OnGridObjectChanged += OnGridObjectChanged;
 
-            for (int i = 0; i < seed.cells.Length; i++) {
+            for (int i = 0; i < seed.cells.Count; i++) {
                 int value;
                 grid.GetXZ(i, out int x, out int z);
 
                 if (seed.cells[i] != "") {
                     if (int.TryParse(seed.cells[i], out value)) {
+                        value++;
                         if (value < tilesArray.Length) {
                             if (tilesArray[value] != null) {
                                 Transform newCellTransform = Instantiate(tilesArray[value], mapParent).transform;
@@ -113,6 +149,12 @@ public class MapManager : MonoBehaviour {
                     }
                 } else {
                     //Debug.Log($"The cell n.{i} is empty.");
+                    if (tilesArray[0] != null) {
+                        Transform newCellTransform = Instantiate(tilesArray[0], mapParent).transform;
+                        newCellTransform.localPosition = grid.GetWorldPosition(x, z);
+                    } else {
+                        Debug.LogWarning($"Blank tile prefab is null.");
+                    }
                 }
             }
 
@@ -124,7 +166,10 @@ public class MapManager : MonoBehaviour {
     public void LoadMapAround(string seed, Vector3 originPosition) {
 
         Seed mapSeed = new Seed(seed);
-        LoadMap(mapSeed, ((new Vector3(-mapSeed.width / 2.0f + 0.5f, 0, -mapSeed.height / 2.0f + 0.5f) + originPosition) * C_CellSize));
+        if (mapSeed.isValid) {
+            mapSeed.AddBorder(borderDepth);
+            LoadMap(mapSeed, (new Vector3(-mapSeed.width / 2.0f + 0.5f, 0, -mapSeed.height / 2.0f + 0.5f) * C_CellSize));
+        }
 
     }
 
---
+++ Assets/Scripts/Utils/MyUtils.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public static class MyUtils {
+    
+    public static void AddBlankRange(this List<string> list, int count) {
+        if (count > 0) {
+            for (int i = 0; i < count; i++) {
+                list.Add("");
+            }
+        }
+    }
+
+}
+
+public static class UIUtils {
+
+    public static TextMesh CreateWorldText(string text, Transform parent, Vector3 localPosition, Quaternion localRotation, Color fontColor, int fontSize = 40, TextAnchor textAnchor = TextAnchor.MiddleCenter) {
+        if (fontColor == null) fontColor = Color.white;
+        GameObject gameObject = new GameObject("World_Text", typeof(TextMesh));
+        Transform transform = gameObject.transform;
+        transform.SetParent(parent, false);
+        transform.localPosition = localPosition;
+        transform.localRotation = localRotation;
+        TextMesh textMesh = gameObject.GetComponent<TextMesh>();
+        textMesh.anchor = textAnchor;
+        textMesh.fontSize = fontSize;
+        textMesh.color = fontColor;
+        textMesh.font = GetDefaultFont();
+        textMesh.text = text;
+        return textMesh;
+    }
+
+    // Get Default Unity Font, used in text objects if no font given
+    public static Font GetDefaultFont() {
+        return Resources.GetBuiltinResource<Font>("Arial.ttf");
+    }
+
+}
+
+public static class EditorUtils {
+
+    // Method made by Andeeeee
+    // Source: https://forum.unity.com/threads/giving-unitygui-elements-a-background-color.20510/#post-422235
+    public static Texture2D MakeTex(int width, int height, Color col) {
+        Color[] pix = new Color[width * height];
+
+        for (int i = 0; i < pix.Length; i++)
+            pix[i] = col;
+
+        Texture2D result = new Texture2D(width, height);
+        result.SetPixels(pix);
+        result.Apply();
+
+        return result;
+    }
+
+}
+
+public static class DirectionUtils {
+
+    public static Direction Opposite(this Direction dir) {
+        switch (dir) {
+            default:
+            case Direction.Up: return Direction.Down;
+            case Direction.Right: return Direction.Left;
+            case Direction.Down: return Direction.Up;
+            case Direction.Left: return Direction.Right;
+        }
+    }
+
+    public static void ToCoord(this Direction dir, ref int x, ref int z) {
+        if (dir != Direction.NULL) {
+            switch (dir) {
+                default:
+                case Direction.Up: z++; break;
+                case Direction.Right: x++; break;
+                case Direction.Down: z--; break;
+                case Direction.Left: x--; break;
+            }
+        } else {
+            x = -1;
+            z = -1;
+        }
+    }
+
+    public static float ToAngle(this Direction dir) {
+        float result = -1;
+
+        if (dir != Direction.NULL) {
+            result = (int)dir * 90;
+        }
+
+        return result;
+    }
+}
