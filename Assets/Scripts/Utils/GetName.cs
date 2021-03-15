4fb4e817557e1c227e3a12b87b60bf81bc36b864
vacui <graizzaromatteo@gmail.com>
2021-03-15 16:03:46 +0100

    refactor: add first draft for create level panel

---
+++ Assets/Scripts/Utils/GetName.cs
@@ -0,0 +1,26 @@
+﻿using TMPro;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(TextMeshProUGUI))]
+public class GetName : MonoBehaviour
+{
+    [SerializeField] private Transform parent;
+
+#if (UNITY_EDITOR)
+    private void Awake()
+    {
+        parent = transform.parent;
+    }
+
+    private void Update()
+    {
+        GetParentName();
+    }
+
+    private void GetParentName()
+    {
+        if (parent != null) GetComponent<TextMeshProUGUI>().text = parent.name;
+    }
+#endif
+}
