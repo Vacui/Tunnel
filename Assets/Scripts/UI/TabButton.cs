fd668bbbaab440cd22cb5179da5135a4a1fc12a9
vacui <graizzaromatteo@gmail.com>
2021-03-16 14:02:08 +0100

    style: map size settings UI always accessible

--- Assets/Scripts/UI/TabButton.cs
+++ Assets/Scripts/UI/TabButton.cs
@@ -1,60 +1,84 @@
-﻿using UnityEngine;
-using UnityEngine.UI;
+﻿/* based on tutorial by GameDevGuide
+ * source: https://www.youtube.com/watch?v=211t6r12XPQ
+ * */
+
+ using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(Image))]
-public class TabButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+public class TabButton : MonoBehaviour, IPointerClickHandler
 {
+    [SerializeField] private bool interactable = false;
+    public bool Interactable { get { return interactable; } }
+
     [SerializeField] private TabGroup tabGroup;
-    [SerializeField] private Image background;
+
+    [Header("Colors")]
+    [SerializeField] private Graphic[] graphics;
+    [SerializeField] private Color[] activeColors;
+    [SerializeField] private Color[] inactiveColors;
+    [SerializeField] private Color[] lockedColors;
+
+    [Header("Game Objects")]
+    [SerializeField] private GameObject[] showOnSelected;
+    [SerializeField] private GameObject[] hideOnSelected;
 
     private void Awake()
     {
         if (tabGroup != null)
-            SubscribeToTabGroup();
-    }
-
-    public void SetTabGroup(TabGroup tabGroup)
-    {
-        if(tabGroup == null)
-        {
-            this.tabGroup = tabGroup;
-            SubscribeToTabGroup();
-        }
-    }
-
-    private void SubscribeToTabGroup()
-    {
-        tabGroup.Subscribe(this);
-    }
-
-    public void OnPointerEnter(PointerEventData eventData)
-    {
-        if (tabGroup != null)
-            tabGroup.OnTabEnter(this);
-    }
-
-    public void OnPointerExit(PointerEventData eventData)
-    {
-        if (tabGroup != null)
-            tabGroup.OnTabExit(this);
+            tabGroup.Subscribe(this);
     }
 
     public void OnPointerClick(PointerEventData eventData)
     {
-        if (tabGroup != null)
+        if (tabGroup != null && interactable)
             tabGroup.OnTabSelected(this);
     }
 
-    public void SetBackgroundSprite(Sprite sprite)
+    private void UpdateGraphic(Color[] colors, bool show)
     {
-        if (background != null)
-            background.sprite = sprite;
+        for (int i = 0; i < graphics.Length; i++)
+        {
+            if (graphics[i] != null)
+                if (i < colors.Length)
+                    graphics[i].color = colors[i];
+        }
+
+        for (int i = 0; i < showOnSelected.Length; i++)
+        {
+            if (showOnSelected[i] != null)
+                showOnSelected[i].SetActive(show);
+        }
+
+        for (int i = 0; i < hideOnSelected.Length; i++)
+        {
+            if (hideOnSelected[i] != null)
+                hideOnSelected[i].SetActive(!show);
+        }
     }
 
-    public void SetBackgroundColor(Color color)
+    public void Select()
     {
-        if (background != null)
-            background.color = color;
+        if (interactable)
+            UpdateGraphic(activeColors, true);
+    }
+
+    public void Deselect()
+    {
+        if (interactable)
+            UpdateGraphic(inactiveColors, false);
+    }
+
+    public void Lock()
+    {
+        interactable = false;
+        UpdateGraphic(lockedColors, false);
+    }
+
+    public void Unlock()
+    {
+        interactable = true;
+        Deselect();
     }
 }
--- Assets/Scripts/UI/TabGroup.cs
+++ Assets/Scripts/UI/TabGroup.cs
@@ -1,121 +1,49 @@
-﻿using System.Collections.Generic;
+﻿/* based on tutorial by GameDevGuide
+ * source: https://www.youtube.com/watch?v=211t6r12XPQ
+ * */
+
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Events;
 
 public class TabGroup : MonoBehaviour
 {
-    public enum VisualState
-    {
-        Idle,
-        Hovered,
-        Active
-    }
-
-    [System.Serializable]
-    public enum Transition
-    {
-        SpriteSwap,
-        ColorTint
-    }
-
-    [System.Serializable]
-    public class UnityTabButtonEvent : UnityEvent<TabButton> { }
-
-    [SerializeField] private UnityTabButtonEvent OnTabButtonChanged;
-
     private List<TabButton> tabButtons;
     private TabButton selectedButton;
-
-    [SerializeField] private Transition transition;
-
-    [ShowIf(nameof(transition), Transition.SpriteSwap)]
-    [SerializeField] private Sprite tabSpriteIdle;
-    [ShowIf(nameof(transition), Transition.SpriteSwap)]
-    [SerializeField] private Sprite tabSpriteHover;
-    [ShowIf(nameof(transition), Transition.SpriteSwap)]
-    [SerializeField] private Sprite tabSpriteActive;
-
-    [ShowIf(nameof(transition), Transition.ColorTint)]
-    [SerializeField] private Color tabColorIdle;
-    [ShowIf(nameof(transition), Transition.ColorTint)]
-    [SerializeField] private Color tabColorHover;
-    [ShowIf(nameof(transition), Transition.ColorTint)]
-    [SerializeField] private Color tabColorActive;
+    [SerializeField] PanelGroup panelGroup;
 
     public void Subscribe(TabButton button)
     {
         if (tabButtons == null)
             tabButtons = new List<TabButton>();
 
-        if(button != null)
-        {
+        if (button != null)
             if (!tabButtons.Contains(button))
-            {
                 tabButtons.Add(button);
-                SetTabButtonVisual(button, VisualState.Idle);
-            }
-        }
-    }
 
-    public void OnTabEnter(TabButton button)
-    {
-        if (button != null && button != selectedButton)
-            SetTabButtonVisual(button, VisualState.Hovered);
-    }
-
-    public void OnTabExit(TabButton button)
-    {
-        if(button != null && button != selectedButton)
-            SetTabButtonVisual(button, VisualState.Idle);
+        if (button.Interactable)
+        {
+            if (selectedButton == null)
+                OnTabSelected(button);
+            else
+                button.Deselect();
+        } else
+            button.Lock();
     }
 
     public void OnTabSelected(TabButton button)
     {
-        if (button != null)
+        if (button != null && button != selectedButton)
         {
-            if(selectedButton != null)
-                if(button != selectedButton)
-                    SetTabButtonVisual(selectedButton, VisualState.Idle);
+            if (selectedButton != null)
+            {
+                selectedButton.Deselect();
+            }
 
             selectedButton = button;
-            SetTabButtonVisual(selectedButton, VisualState.Active);
+            selectedButton.Select();
 
-            OnTabButtonChanged?.Invoke(selectedButton);
+            if (panelGroup != null)
+                panelGroup.SetPanelIndex(selectedButton.transform.GetSiblingIndex());
         }
-    }
-
-    private void SetTabButtonVisual(TabButton button, VisualState state)
-    {
-        switch (transition)
-        {
-            case Transition.SpriteSwap:
-                SetTabButtonSpriteVisual(button, state);
-                break;
-            case Transition.ColorTint:
-                SetTabButtonColorVisual(button, state);
-                break;
-        }
-    }
-
-    public void SetTabButtonSpriteVisual(TabButton button, VisualState state)
-    {
-        Sprite sprite = tabSpriteIdle;
-        switch (state)
-        {
-            case VisualState.Hovered: sprite = tabSpriteHover; break;
-            case VisualState.Active: sprite = tabSpriteActive; break;
-        }
-        button.SetBackgroundSprite(sprite);
-    }
-
-    public void SetTabButtonColorVisual(TabButton button, VisualState state)
-    {
-        Color color = tabColorIdle;
-        switch (state)
-        {
-            case VisualState.Hovered: color = tabColorHover; break;
-            case VisualState.Active: color = tabColorActive; break;
-        }
-        button.SetBackgroundColor(color); 
     }
 }
