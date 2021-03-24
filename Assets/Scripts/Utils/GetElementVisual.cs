0ddb2ba97f85fc4ce7c1a301204fee85ca46c40c
vacui <graizzaromatteo@gmail.com>
2021-03-24 21:42:43 +0100

    style: add util script to auto update editor UI elements images

---
+++ Assets/Scripts/Utils/GetElementVisual.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(Image))]
+public class GetElementVisual : MonoBehaviour
+{
+    [SerializeField] private ElementsVisuals visuals;
+    [SerializeField] private TileType tileType;
+
+    private void Awake()
+    {
+        GetElementVisualToSprite();
+    }
+
+#if (UNITY_EDITOR)
+    private void Update()
+    {
+        GetElementVisualToSprite();
+    }
+#endif
+
+    private void GetElementVisualToSprite()
+    {
+        if (visuals != null) GetComponent<Image>().sprite = visuals.GetVisual(tileType);
+    }
+}
