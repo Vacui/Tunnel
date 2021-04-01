19885e3e57fc38026fb24f9fa6ada6d1f99318b3
vacui <graizzaromatteo@gmail.com>
2021-04-02 00:41:10 +0200

    style: remove unnecessary usings in MyUtils class

--- Assets/Scripts/Utils/MyUtils.cs
+++ Assets/Scripts/Utils/MyUtils.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Reflection;
-using UnityEditor;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public static class MyUtils
