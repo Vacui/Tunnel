1253134f559b15f4ef104880afa501e1e6edb500
vacui <graizzaromatteo@gmail.com>
2021-04-12 18:02:41 +0200

    style: add navbar to new level UI

---
+++ Assets/Scripts/Utils/ForceRebuildLayoutImmediate.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(RectTransform))]
+public class ForceRebuildLayoutImmediate : MonoBehaviour
+{
+    [SerializeField] private bool OnAwakeObj;
+    [SerializeField] private bool OnEnableObj;
+
+    private void Awake() { if (OnAwakeObj) LayoutRebuilder.ForceRebuildLayoutImmediate(GetComponent<RectTransform>()); }
+    private void OnEnable() { if (OnEnableObj) LayoutRebuilder.ForceRebuildLayoutImmediate(GetComponent<RectTransform>()); }
+}
